4acbc0b274daae0d3f1bf9b91f2be0a8f42b969f
Gabrieldo0800 <gabrielleardinicanassa@gmail.com>
2024-08-02 10:39:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$nome = 'Gabriel ';
+$escola = 'SESI Cerquilho ';
+$CE = 428 ;
+$curso = 'Desenvolvimento de Sistemas ';
+$media = 9.5 ;
+
+echo $nome . '<br>';
+echo $escola . '<br>';
+echo $CE . '<br>';
+echo $curso . '<br>';
+echo $media . '<br>';
+
+echo "ola $nome, seja bem vindo ao $escola, da unidade escolar $CE, cursando o curso de $curso"
+?>
